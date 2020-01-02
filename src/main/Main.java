033ee51a777bb5ca8f0f60ed4e47d2a5bd5bbf8a
authomas <aurelien.thomas@capgemini.com>
2020-01-02 13:50:04 +0100

    create rectangle or ellipse

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,39 +6,33 @@ import fr.dgac.ivy.IvyException;
 import fr.dgac.ivy.IvyMessageListener;
 
 public class Main {
-    public static void main (String arg[]) {
+    private static boolean isClicked = false;
+    private static int x, y;
+    private static String formeFonc = "";
+    private static String couleur = "";
 
+    public static void main(String[] args) {
         Ivy busIvy = new Ivy("monBus", "Premier message de monBusJava", null);
+
 
         try {
             busIvy.start("127.255.255.255:2010");
-            busIvy.bindMsg("^Palette:(.*)$", new IvyMessageListener() {
+
+            busIvy.bindMsg("^OneDollar Reco=(.*)$", new IvyMessageListener() {
                 @Override
                 public void receive (IvyClient client, String[] args) {
-
-                    //1. Afficher tous les messages précédés du nom de l’agent qui l’a envoyé
-                    System.out.println("Palette : " + args[0]);
-                }
-            });
-
-            busIvy.bindMsg("^Palette:MouseClicked x=(.*) y=(.*)$", new IvyMessageListener() {
-                @Override
-                public void receive (IvyClient client, String[] args) {
+                        if(args[0].equals("rectangle")){
+                            formeFonc = "CreerRectangle";
+                        }else if(args[0].equals("ellipse")){
+                            formeFonc ="CreerEllipse";
+                        }else{
+                            formeFonc+="PasReconnus";
+                        }
+                    //x=" + x + " y=" + y
                     try {
-                        //2. Envoyer  un  message sur  le  bus  IVY  de  manière  à  faire  afficher  un  rectangle  sur  la palette
-                        busIvy.sendMsg("Palette:CreerRectangle x=" + args[0] + " y=" + args[1]);
-                    } catch (IvyException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-            busIvy.bindMsg("^Palette:MouseMoved x=(.*) y=(.*)$", new IvyMessageListener() {
-                @Override
-                public void receive (IvyClient client, String[] args) {
-                    try {
-                        //3. Afficher seulement les messages de déplacement du pointeur de la souris sur la palette.Le  message  affiché  devra  être  de  la  forme:  «Le  pointeur  de  la  souris  se  trouve  à  la position (coordX, coordY)» avec coordX et coordYles coordonnées transmises dans le message.
-                        busIvy.sendMsg("Le  pointeur  de  la  souris  se  trouve  à  la position (" + args[0] + "," + args[1] + ")");
+                        busIvy.sendMsg("Palette:"+formeFonc+" x="+x+" y="+y+ couleur);
+                        x = 250;
+                        y = 250;
                     } catch (IvyException e) {
                         e.printStackTrace();
                     }
@@ -49,4 +43,5 @@ public class Main {
             e.printStackTrace();
         }
     }
+
 }
